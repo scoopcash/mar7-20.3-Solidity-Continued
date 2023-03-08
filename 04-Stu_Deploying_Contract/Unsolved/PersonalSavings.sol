8e4b3a1ab4a45aaa4b7c55310fa43d943866d85c
scoopcash <114896185+scoopcash@users.noreply.github.com>
2023-03-07 21:13:26 -0500

    done

--- 04-Stu_Deploying_Contract/Unsolved/PersonalSavings.sol
+++ 04-Stu_Deploying_Contract/Unsolved/PersonalSavings.sol
@@ -65,14 +65,36 @@ contract PersonalSavings {
   address payable public public_savings = 0x77B2aD074a1aF1AF2a408E3D48465E5F9ec75f45;
   address payable private_savings = 0x873Ad450656C46cb564eaf418472A1c77d7327Ad;
   string account_holder = "Jane Doe";
+  address last_to_withdraw;
+  uint last_withdraw_amount;
+  address last_to_deposit;
+  uint public balance;
+  uint last_deposit_amount;
+
 
   function withdraw(uint amount, address payable recipient) public {
-    if (recipient == public_savings || recipient == private_savings && address(this).balance >= amount) {
-      return recipient.transfer(amount);
+    require(recipient == public_savings || recipient == private_savings, "This is not your account");
+    require( address(this).balance >= amount, "You don't have enough funds!");
+    
+    if (last_to_withdraw != recipient) {
+      last_to_withdraw = recipient;
     }
+    
+    last_withdraw_amount = amount; 
+
+    balance = address(this).balance - last_withdraw_amount;
+
+    return msg.sender.transfer(amount);
+    
   }
 
   function deposit() public payable {
+    if (last_to_deposit != msg.sender){
+      last_to_deposit = msg.sender;
+    }
+
+    balance = address(this).balance;
+    last_deposit_amount = msg.value;
   }
 
   function() external payable {
